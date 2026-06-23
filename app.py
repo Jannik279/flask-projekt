ecc04be427b3c8d8f38fd10a583aa77e29107efd
Jannik279 <roettgerjannik7@gmail.com>
2026-06-23 22:50:25 +0200

    Add round_id to track game rounds

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, request, jsonify
-import time
 
 app = Flask(__name__)
 
 answers = {"1": None, "2": None, "3": None}
 answer_order = []
-round_active = True
+round_id = 0  # 👈 NEU
 
 
 @app.route("/")
@@ -22,23 +21,16 @@ def submit(ipad):
 
 @app.route("/save", methods=["POST"])
 def save():
-    global round_active, answer_order
+    global answer_order
 
     data = request.get_json()
     ipad = str(data.get("ipad"))
     answer = data.get("answer")
 
-    if not round_active:
-        return jsonify({"success": False, "msg": "Runde beendet"})
-
     if answers[ipad] is not None:
-        return jsonify({"success": False, "msg": "Schon geantwortet"})
+        return jsonify({"success": False})
 
-    answers[ipad] = {
-        "answer": answer,
-        "time": time.time()
-    }
-
+    answers[ipad] = answer
     answer_order.append(ipad)
 
     return jsonify({"success": True})
@@ -49,16 +41,18 @@ def state():
     return jsonify({
         "answers": answers,
         "order": answer_order,
-        "round_active": round_active
+        "round_id": round_id   # 👈 WICHTIG
     })
 
 
 @app.route("/reset", methods=["POST"])
 def reset():
-    global answers, answer_order, round_active
+    global answers, answer_order, round_id
+
     answers = {"1": None, "2": None, "3": None}
     answer_order = []
-    round_active = True
+    round_id += 1   # 👈 neue Runde!
+
     return jsonify({"success": True})
 
 
